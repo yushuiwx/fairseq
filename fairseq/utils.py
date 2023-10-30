613b50551064637d7f0fdbf4b49536ebab1a0423
ustcwhy <wanghongyu22@mails.ucas.ac.cn>
2023-10-30 08:55:59 +0000

    fx

--- fairseq/utils.py
+++ fairseq/utils.py
@@ -543,6 +543,8 @@ def get_activation_fn(activation: str) -> Callable:
         return torch.tanh
     elif activation == "linear":
         return lambda x: x
+    elif activation == "silu":
+        return F.silu
     else:
         raise RuntimeError("--activation-fn {} not supported".format(activation))
 
@@ -555,6 +557,7 @@ def get_available_activation_fns() -> List:
         "gelu_accurate",
         "tanh",
         "linear",
+        "silu",
     ]
 
 
