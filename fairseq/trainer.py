c73dea3d42057be5bfc237287feace3eed3e7c43
Shuming Ma <shumma@microsoft.com>
2024-03-24 23:21:36 -0700

    remove hardcode for group zero

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -157,7 +157,10 @@ class Trainer(object):
             if self.is_moe:
                 _, self.expert_group = get_moe_group(self.cfg.model.moe_expert_count)
             else:
-                self.expert_group = get_zero_group(torch.cuda.device_count() * 2) # self.data_parallel_process_group
+                if self.cfg.distributed_training.zero_group_size > 0:
+                    self.expert_group = get_zero_group(torch.cuda.device_count() * 2)
+                else:
+                    self.expert_group = self.data_parallel_process_group
 
         # TODO(myleott): support tpu
         if self.cuda and self.data_parallel_world_size > 1:
